3af6db47c12752150553a20e7e065bd1bbab5b6f
Akabhishek067 <akabhishek067@gmail.com>
2022-06-24 17:12:14 +0530

    change detection example added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -26,10 +27,35 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-testing');
   });
 
-  it('should render title', () => {
+  xit('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('angular-testing app is running!');
   });
+
+  it('should not contain any name initially', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.userName).toEqual('');
+  });
+
+  it('should return the value set in userName', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.setUserName('mobile-man')
+    expect(app.userName).toEqual('mobile-man');
+  });
+
+  it('should change Name in dom', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const el = fixture.debugElement.query(By.css('p'));
+    expect(el.nativeElement.textContent.trim()).toBe('');
+    app.setUserName('mobile-man');
+    expect(el.nativeElement.textContent.trim()).toBe('');
+    fixture.detectChanges();
+    expect(el.nativeElement.textContent.trim()).toBe('Name: mobile-man');
+
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-testing';
+
+  userName: string ='';
+
+  setUserName = (userName: string):void => {
+    this.userName = userName;
+  }
 }
