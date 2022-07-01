da7d5ea06837a2a405d615faf5ff24510e79924a
Akabhishek067 <akabhishek067@gmail.com>
2022-07-01 09:19:58 +0530

    form added in html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -6,7 +8,9 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule,
+        FormsModule
       ],
       declarations: [
         AppComponent
@@ -14,22 +18,39 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  // it('should submit employee details', () => {
+    
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const component = fixture.componentInstance;
+
+  //   fixture.detectChanges();
+  //   let formInput = fixture.debugElement.queryAll(By.css('input'));
+
+  //   let userName = formInput[0].nativeElement;
+  //   let phoneNo = formInput[1].nativeElement;
+  //   let address = formInput[2].nativeElement;
+  //   let submitBtn = fixture.debugElement.query(By.css('button')).nativeNode;
+    
+  //   userName.value = 'abhishek';
+  //   phoneNo.value = 1234567890;
+  //   address.value = 'New Delhi';
+
+  //   submitBtn.click();
+    
+  //   fixture.detectChanges();
+
+  //   console.log('component value', component.empDetails.value);
+  // })
+
+  it('should submit employee details', async () => {
+    
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+
+    let formInput = fixture.debugElement.queryAll(By.css('input'));
+    let userName = formInput[0].nativeElement;
+    userName.value = 'abhishek';
+
   });
 
-  it(`should have as title 'angular-testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-testing');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-testing app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-testing';
+
+  nameVal: string = '';
+  empDetails;
+
+  constructor(private fb: FormBuilder) {
+    this.empDetails = fb.group({
+      empName: [''],
+      empPhoneNo: [''],
+      empAddress: ['']
+    })
+  }
+
+  submitEmpDetails = () => {
+    console.log(this.empDetails.value);
+    this.nameVal = this.empDetails.value.userName;
+    
+  }
+
 }
